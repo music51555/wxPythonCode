c6ecedd8f9da6c5375493c07889828ce3b440e63
music51556 <music51555@163.com>
2018-12-05 17:35:33 +0800

    1205gs

--- m6/MyBlog/blog/views.py
+++ m6/MyBlog/blog/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render,HttpResponse
+from PIL import Image,ImageDraw,ImageFont
+import random
+import os
+from io import BytesIO
+import string
+
+BASE_DIR=os.path.dirname(os.path.dirname(__file__))
+valid_img_bgc=os.path.join(BASE_DIR,'static','valid_img_bgc','valid_bgc.png')
 
 def login(request):
 
     return render(request,'login.html')
 
 def get_validCode_img(request):
-
-    # 对于验证码图片，直接打开一张图片返回，这并不合适，因为图片是固定的
-    # with open('lufei.jpeg','rb') as f:
-    #     data=f.read()
-    import random
     def get_color():
-        return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-    from PIL import Image
     img=Image.new('RGB',(270,40),color=get_color())
 
-    with open('ValidCode_bgc.png','wb') as f:
-        img.save(f,'png')
+    # 表示通过ImageDraw画笔，画在哪个图片上
+    draw=ImageDraw.Draw(img)
 
-    with open('ValidCode_bgc.png','rb') as f:
-        data=f.read()
+    # 根据ttf字体文件创建字体对象，并设置字体大小
+    fzxzt_font=ImageFont.truetype('static/font/KumoFont.ttf',size=30)
+
+    for i in range(5):
+        rand_str = ''.join(random.sample(string.ascii_letters + str(random.randint(0, 9)), 1))
+        # 在图片上可以写字，可以划线，可以写点，分别传入(坐标，字符，文字颜色，字体对象)
+        draw.text((i*60,3),rand_str,fill=get_color(),font=fzxzt_font)
+
+    f=BytesIO()
+    img.save(f,'png')
+    data=f.getvalue()
 
     return HttpResponse(data)
