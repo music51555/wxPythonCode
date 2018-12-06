834911546727ed0347448d1e56905b38a803b137
music51556 <music51555@163.com>
2018-12-06 20:26:46 +0800

    1206gs

--- m6/MyBlog/blog/views.py
+++ m6/MyBlog/blog/views.py
@@ -13,21 +13,20 @@ def login(request):
     return render(request,'login.html')
 
 def get_validCode_img(request):
+
     def get_color():
-        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
     img=Image.new('RGB',(270,40),color=get_color())
 
-    # 表示通过ImageDraw画笔，画在哪个图片上
     draw=ImageDraw.Draw(img)
 
-    # 根据ttf字体文件创建字体对象，并设置字体大小
-    fzxzt_font=ImageFont.truetype('static/font/KumoFont.ttf',size=30)
+    font=ImageFont.truetype('static/font/KumoFont.ttf',size=30)
 
     for i in range(5):
-        rand_str = ''.join(random.sample(string.ascii_letters + str(random.randint(0, 9)), 1))
-        # 在图片上可以写字，可以划线，可以写点，分别传入(坐标，字符，文字颜色，字体对象)
-        draw.text((i*60,3),rand_str,fill=get_color(),font=fzxzt_font)
+        rand_str=''.join(random.sample(string.ascii_letters+str(random.randint(0,9)),1))
+        print(rand_str)
+        draw.text((i*50,5),rand_str,fill=get_color(),font=font)
 
     f=BytesIO()
     img.save(f,'png')
